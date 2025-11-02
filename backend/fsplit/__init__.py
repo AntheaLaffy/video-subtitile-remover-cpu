763867a5b4441ed6f418dcc532e50d5fea81013a
如月风铃 <NicoleSandwich@outlook.com>
2025-11-02 18:02:23 +0800

    Add files via upload

---
+++ backend/fsplit/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+##
+# fsplit
+# https://github.com/leosartaj/fsplit.git
+#
+# Copyright (c) 2014 Sartaj Singh
+# Licensed under the MIT license.
+##
+
+#from info import __version__ # define __version__ variable
+__version__ = '1.0.0'
+#from info import __desc__ # define __desc__ variable for description
+__desc__ = 'Split files into small portable chunks' 
